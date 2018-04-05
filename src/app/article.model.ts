4c440e745dd116cd267a0c7437d3dbf5882baab4
Deviza Vasile <deviza.vasile@protonmail.com>
2018-04-05 14:31:08 +0300

    Date and showDate added
    Date and showDate added to Article model class

--- src/app/article.model.ts
+++ src/app/article.model.ts
@@ -11,6 +11,12 @@ mainImgUrl : All articles should have at least one image, this variable stores t
 
 content : All text will be stored in this variable.
 
+tagsList : Contains all tags for this article .
+
+date : Date then the article was writen.
+
+showDate : Date to display.
+
 
  */
 
@@ -20,13 +26,14 @@ export class Article {
 
     public shortText:string;
     public tags:Tags;
+    public date:string;
+    public showDate:string;
 
-    constructor( public title:string , public mainImgUrl:string , public content:string, public tagsList:string[] )
+    constructor( public title:string , public mainImgUrl:string , public content:string, public tagsList:string[], public dateObject = new Date()  )
     {  
         // generates text for theading  
         // majority of publication uses 30 words as short description
         //in headings.
-
 
         let words = content.split(' ');
         let contentLengt = words.length
@@ -43,6 +50,25 @@ export class Article {
 
         this.tags = new Tags(this.tagsList);
 
+        //getting date then article was writed
+            var month = new Array();
+            month[0] = "January";
+            month[1] = "February";
+            month[2] = "March";
+            month[3] = "April";
+            month[4] = "May";
+            month[5] = "June";
+            month[6] = "July";
+            month[7] = "August";
+            month[8] = "September";
+            month[9] = "October";
+            month[10] = "November";
+            month[11] = "December";
+
+            this.date=dateObject.getFullYear()+" "+dateObject.getMonth()+" "+dateObject.getDate();
+
+            this.showDate=dateObject.getFullYear()+" "+month[dateObject.getMonth()]+" "+dateObject.getDate();
+            
      };
 
 
@@ -71,6 +97,16 @@ export class Article {
      public getTags()
      {
          return this.tags
+     }
+
+     public getDate()
+     {
+         return this.date
+     }
+
+     public getShowDate()
+     {
+         return this.showDate;
      }
 
 
@@ -106,6 +142,14 @@ export class Article {
      {
          this.tags.removeTag(toRemoveTag);
      }
+
+     //please use the following format (YYYY MM DD)
+     public setDate(newDate:string)
+     {
+         this.showDate=newDate;
+     }
+
+
 
 
 }
--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 
 
 
@@ -11,9 +12,27 @@ import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+  
+  options: Object = {
+    placeholderText: 'Edit Your Content Here!',
+    charCounterCount: false
+  }
+
+  public editorContent: string = ''
+
+
+  clickMessage = '';
+  onClickMe() {
+    this.clickMessage = this.editorContent;
+
+  }
+
+  constructor() { 
+
+  }
 
   ngOnInit() {
+
   }
 
 }
