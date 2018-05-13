1be44dd2230fa149a8293b37ae9dccdcdc6822f2
DevizaVasile <deviza.vasile.protonmail.com>
2018-05-13 21:05:19 +0300

    nothing

--- src/app/article.model.ts
+++ src/app/article.model.ts
@@ -30,7 +30,6 @@ export class Article {
     public showDate:string;
     public $key:string;
     
-    
 
     constructor(public title:string , public mainImgUrl:string , public content:string, public tagsList:string[])
     {  
